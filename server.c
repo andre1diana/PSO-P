756ec35e76b83c576445856a286df60f4bcf81dd
andre1diana <andrei.diana370@gmail.com>
2024-12-07 18:48:46 +0200

    update

--- server.c
+++ server.c
@@ -178,17 +178,17 @@ void handle_client_message(int socket) {
     switch(header.type) {
         case MSG_AGENT_REGISTER: {
             AgentRegistration* reg = (AgentRegistration*)payload_buffer;
-            // Procesează înregistrarea agentului
+            // Proceseaza inregistrarea agentului
             break;
         }
         case MSG_TASK_SUBMIT: {
             TaskSubmission* task = (TaskSubmission*)payload_buffer;
-            // Procesează task-ul primit
+            // Proceseaza task-ul primit
             break;
         }
         case MSG_TASK_RESULT: {
             TaskResult* result = (TaskResult*)payload_buffer;
-            // Procesează rezultatul
+            // Proceseaza rezultatul
             break;
         }
     }
